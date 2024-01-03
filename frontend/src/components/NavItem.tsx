bd78fb0671e64dfcdc27a96ecf22ae1a2c9910c7
oscarprdev <vlcoscar8@gmail.com>
2024-01-03 14:39:35 +0100

    feat: (nav) add nav-item and icons with basic routing

---
+++ frontend/src/components/NavItem.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { Link, useLocation } from 'wouter';
+
+interface NavItemProps {
+	text: string;
+	defaultIcon: ReactNode;
+	activeIcon: ReactNode;
+}
+
+const NavItem = ({ text, defaultIcon, activeIcon }: NavItemProps) => {
+	const [isActive, setIsActive] = useState(false);
+	const [location] = useLocation();
+
+	useEffect(() => {
+		if (location.match(text.toLowerCase().slice(1, text.length))) {
+			setIsActive(true);
+		} else {
+			setIsActive(false);
+		}
+	}, [location, text]);
+
+	return (
+		<Link
+			href={`/${text.toLowerCase()}`}
+			className='flex items-center justify-start gap-2 py-2 pr-8 px-4 w-fit rounded-full hover:bg-zinc-900'>
+			<span className='block w-10'>{isActive ? activeIcon : defaultIcon}</span>
+			<p className={`capitalize text-xl ${isActive ? 'font-bold' : 'font-light'}`}>{text}</p>
+		</Link>
+	);
+};
+
+export default NavItem;
