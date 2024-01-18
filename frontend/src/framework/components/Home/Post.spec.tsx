40ef5b10c8bf788ec94bf04d3f8795ab0b356c65
oscarprdev <vlcoscar8@gmail.com>
2024-01-18 09:52:21 +0100

    Fix unit tests with userLogger handler at Post.spec.tsx and Nav.spec.tsx

--- frontend/src/framework/components/Home/Post.spec.tsx
+++ frontend/src/framework/components/Home/Post.spec.tsx
@@ -1,13 +1,18 @@
 import { RenderResult, render } from '@testing-library/react';
 import Post from './Post';
-import { afterEach, beforeEach, describe, it } from 'vitest';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, it } from 'vitest';
 import { strDateToTime } from '../../utils/strDateToTime';
 import { postResponse } from '../../../tests/unit/responses/posts.response';
 import { Provider } from 'react-redux';
 import { mockStore } from '../../../tests/unit/store/store.mock';
+import { server } from '../../../tests/unit/server/server.mock';
+import { testUserLoggedHandler } from '../../../tests/unit/handlers/users.handlers';
 
 describe('Post', () => {
 	let component: RenderResult;
+
+	beforeAll(() => server.listen());
+	afterAll(() => server.close());
 
 	beforeEach(() => {
 		component = render(
@@ -15,9 +20,14 @@ describe('Post', () => {
 				<Post post={postResponse} />
 			</Provider>
 		);
+
+		server.use(testUserLoggedHandler);
 	});
 
-	afterEach(() => component.unmount());
+	afterEach(() => {
+		server.resetHandlers();
+		component.unmount();
+	});
 
 	it('should render successfully', () => {
 		component.getByText(`${postResponse.owner.name} ${postResponse.owner.surname}`);
