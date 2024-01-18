d49b19e6f0a8cfff4d7e9b2c0355e1b62d1c0ede
oscarprdev <vlcoscar8@gmail.com>
2024-01-18 09:52:00 +0100

    Add userLogged mock server handler

--- frontend/src/tests/unit/handlers/users.handlers.ts
+++ frontend/src/tests/unit/handlers/users.handlers.ts
@@ -1,6 +1,7 @@
 import { HttpResponse, http } from 'msw';
 import { API_URL } from '../../../features/shared/domain/constants/constants';
 import { SuccessfulSearchUsersEmptyResponse, SuccessfulSearchUsersResponse } from '../responses/users/search-users.response';
+import { SuccessfulUserLoggedResponse } from '../responses/users/user-logged-response';
 
 export const testSearchMockUserHandler = http.get(`${API_URL}/users/search/mock`, () => HttpResponse.json(SuccessfulSearchUsersResponse));
 
@@ -8,4 +9,6 @@ export const testSearchMockUserEmptyHandler = http.get(`${API_URL}/users/search/
 	HttpResponse.json(SuccessfulSearchUsersEmptyResponse)
 );
 
-export const usersHandler = [testSearchMockUserHandler, testSearchMockUserEmptyHandler];
+export const testUserLoggedHandler = http.post(`${API_URL}/users/login`, () => HttpResponse.json(SuccessfulUserLoggedResponse));
+
+export const usersHandler = [testSearchMockUserHandler, testSearchMockUserEmptyHandler, testUserLoggedHandler];
