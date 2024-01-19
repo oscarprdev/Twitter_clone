784f6cf1f95cb713aa112777bf595402176a95d6
oscarprdev <vlcoscar8@gmail.com>
2024-01-19 12:08:26 +0100

    Fix constants file

--- frontend/src/features/shared/domain/constants/constants.ts
+++ frontend/src/features/shared/domain/constants/constants.ts
@@ -1,4 +1,5 @@
 export const API_URL = 'https://twitterclone-api.up.railway.app';
+// export const API_URL = 'http://localhost:8080';
 export const CLOUDFLARE_API = 'https://cloudflare-api.vercel.app';
 export const DEFAULT_EMAIL = 'Oscar@email.com';
 export const DEFAULT_PASSWORD = '1234';
